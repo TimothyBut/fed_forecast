cba863aa57a516597a332bc2167ed1afba39939c
apple <apple@But-MacBook.local>
2024-04-07 02:56:39 +0800

    Present Sequence Change: unrate > cpi
    In EDA, it is easier to observe the similarity of unrate with fedfunds than original order

--- Data_List.py
+++ Data_List.py
@@ -4,8 +4,8 @@
 # define a dictionary for desired financial data
 dict_data = {"FEDFUNDS": "Federal Funds Effective Rate",
              "TB3MS": "3-Month Treasury Bill Secondary Market Rate, Discount Basis",
-             "CPIAUCSL":"Consumer Price Index for All Urban Consumers: All Items in U.S. City Average",
              "UNRATE": "Unemployment Rate",
+             "CPIAUCSL":"Consumer Price Index for All Urban Consumers: All Items in U.S. City Average",
              "GDP": "Gross Domestic Product"}
 
 # End of Data List
